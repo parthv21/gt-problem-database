8134a95424b2d430db3742440730173c86035da0
Parth Tamane <parthv21@gmail.com>
2019-12-25 21:44:41 -0500

    fixed link being clickable when not over problem statement

--- src/components/preview/ProblemPreview.js
+++ src/components/preview/ProblemPreview.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import ProblemStatement from "../problem/ProblemStatement";
 import TagContainer from "../tag/TagContainer";
 import MetaInformation from "../problem/MetaInformation";
@@ -15,19 +14,13 @@ const ProblemPreview = ({ problem }) => {
   var date = new Date(problem[attributes.timestamp]);
   return (
     <div key={id} className="problem-preview">
-      <pre>
-        <Link to={"/details/" + id} className="text-link" key={id}>
-          <ProblemStatement problemStatement={problemStatement} />
-        </Link>
-        <MetaInformation title="Sponsor" info={sponsor} />
-        <MetaInformation
-          title="Created On"
-          info={
-            date.getMonth() + "-" + date.getDay() + "-" + date.getFullYear()
-          }
-        />
-        <TagContainer tags={tags} clickable />
-      </pre>
+      <ProblemStatement problemStatement={problemStatement} id={id} clickable />
+      <MetaInformation title="Sponsor" info={sponsor} />
+      <MetaInformation
+        title="Created On"
+        info={date.getMonth() + "-" + date.getDay() + "-" + date.getFullYear()}
+      />
+      <TagContainer tags={tags} clickable />
     </div>
   );
 };
--- src/components/problem/ProblemStatement.js
+++ src/components/problem/ProblemStatement.js
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class ProblemStatement extends Component {
   render() {
-    const { problemStatement } = this.props;
+    const { problemStatement, id, clickable = false } = this.props;
 
-    return <pre className="problem-statement">{problemStatement}</pre>;
+    return (
+      <div className="problem-statement-container">
+        {clickable ? (
+          <Link to={"/details/" + id} className="text-link" key={id}>
+            <span className="problem-statement selectable-problem-statement">
+              {problemStatement}
+            </span>
+          </Link>
+        ) : (
+          <span className="problem-statement">{problemStatement}</span>
+        )}
+      </div>
+    );
   }
 }
 
