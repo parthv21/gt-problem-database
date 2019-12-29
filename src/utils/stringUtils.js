3db350ea7b143de175429b96f50ea49ce3a5ef34
Parth Tamane <parthv21@gmail.com>
2019-12-28 20:51:35 -0500

    added support for escaping regex in search

--- src/utils/stringUtils.js
+++ src/utils/stringUtils.js
@@ -1,16 +1,19 @@
+import _ from "lodash";
+
 const checkForMatch = (
   source,
   searchText,
   matchCaseSensitive,
   matchWholeWord
 ) => {
+  searchText = _.escapeRegExp(searchText);
+
   var regex = searchText;
   regex = matchWholeWord ? "\\b" + regex + "\\b" : regex;
   var options = "g";
   options += !matchCaseSensitive ? "i" : "";
 
-  const matches = new RegExp(regex, options).test(source);
-  return matches;
+  return new RegExp(regex, options).test(source);
 };
 
 const addHighlightSpan = (
@@ -20,20 +23,35 @@ const addHighlightSpan = (
   matchCaseSensitive,
   matchWholeWord
 ) => {
-  var options;
-  options = matchGlobal ? "g" : "";
+  searchText = _.escapeRegExp(searchText);
+
+  var options = matchGlobal ? "g" : "";
   options += !matchCaseSensitive ? "i" : "";
+
   var regex = matchWholeWord
     ? "(\\b" + searchText + "\\b)"
     : "(" + searchText + ")";
-  return source.replace(
+
+  var highlightedSource = source.replace(
     new RegExp(regex, options),
     '<span class="search-highlight">$1</span>'
   );
+
+  const adjSpanFilter = '</span><span class="search-highlight">';
+  highlightedSource = highlightedSource.replace(
+    new RegExp(adjSpanFilter, "g"),
+    ""
+  );
+
+  return highlightedSource;
 };
 
 const stripHtml = source => {
   return source.replace(/<[^>]*>?/gm, "");
 };
 
-export { checkForMatch, addHighlightSpan, stripHtml };
+const unescapeRegex = source => {
+  return source.replace(/\\(.)/g, "$1");
+};
+
+export { checkForMatch, addHighlightSpan, stripHtml, unescapeRegex };
