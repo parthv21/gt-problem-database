89ac627448a1d61b8032bd18763c1424680bf1d8
Parth Tamane <parthv21@gmail.com>
2020-01-21 15:34:33 -0500

    changed open shift bind address

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const tabletop = require("tabletop");
 
 const app = express();
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0";
 
 app.use(express.static(path.join(__dirname, "/client/build")));
 
@@ -25,7 +25,7 @@ app.get("/api/getProblems", (req, res) => {
 });
 
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname + "client/build/index.html"));
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 //"heroku-postbuild": "cd client && npm install --only=dev && npm install && npm run build"
